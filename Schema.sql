d71fc836119dc3f508d64400a1c3db80326619d2
Brookee <oliver.brooke.a@gmail.com>
2019-07-16 13:24:07 -0400

    schema/password update

---
+++ Schema.sql
@@ -0,0 +1,34 @@
+DROP DATABASE IF EXISTS bamazonprods;
+CREATE DATABASE bamazonprods;
+
+USE bamazonprods; 
+
+CREATE TABLE products (
+item_id INT NOT NULL AUTO_INCREMENT,
+product_name TEXT NOT NULL,
+department_name TEXT NOT NULL,
+price DECIMAL(10,2) NOT NULL,
+stock_qty INT NOT NULL,
+PRIMARY KEY(item_id)
+);
+
+INSERT INTO products (product_name, department_name, price, stock_qty) 
+VALUES ("SD Card", "Electronics", 30.00, 50);
+INSERT INTO products (product_name, department_name, price, stock_qty) 
+VALUES ("Hard Drive", "Electronics", 100.00, 5);
+INSERT INTO products (product_name, department_name, price, stock_qty) 
+VALUES ("Backpack", "Travel", 29.99, 10);
+INSERT INTO products (product_name, department_name, price, stock_qty) 
+VALUES ("Pilot Pens", "Home", 7.99, 25);
+INSERT INTO products (product_name, department_name, price, stock_qty) 
+VALUES ("Pillow cases", "Home", 18.99, 12);
+INSERT INTO products (product_name, department_name, price, stock_qty) 
+VALUES ("Laundry Detergent", "Home", 12.99, 18);
+INSERT INTO products (product_name, department_name, price, stock_qty) 
+VALUES ("Nike Free Runs", "Fitness", 45.99, 10);
+INSERT INTO products (product_name, department_name, price, stock_qty) 
+VALUES ("Air Fryer","Kitchen", 99.99, 8);
+INSERT INTO products (product_name, department_name, price, stock_qty) 
+VALUES ("Beats X Headphones", "Electronics", 89.99, 20);
+INSERT INTO products (product_name, department_name, price, stock_qty) 
+VALUES ("Essentia Water", "Food", 18.99, 5);
--- bamazonCustomer.js
+++ bamazonCustomer.js
@@ -1,17 +1,19 @@
 require("dotenv").config();
+var keys = require ("./keys.js");
+
 // downloaded 7/14
 
 var mysql = require("mysql");
 //Both NPM packages downloaded 7/14
 var inquirer = require("inquirer");
 
-// var connected = mysql.createConnection({
-//     host: "localhost",
-//     port: 3306, 
-//     user: "root",
-//     password: "",
-//     database:"bamazonprods"
-// });
+var connected = mysql.createConnection({
+    host: "localhost",
+    port: 3306, 
+    user: "root",
+    password: keys,
+    database:"bamazonprods"
+});
 
 connected.connect(function(error) {
     if(error) throw error;
