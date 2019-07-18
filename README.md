aea176e498e33c32709a02a732b7430660b465ac
Brookee <oliver.brooke.a@gmail.com>
2019-07-17 22:38:38 -0400

    inquire, table display fix. README

--- README.md
+++ README.md
@@ -1,1 +1,15 @@
 # bamazon
+
+Bamazon is a Node.js and MySQL command line Amazon like application that takes in customer orders and depletes the stores inventory
+
+Technology Used 
+* MySQL
+* Node.js
+
+Customer Instuctions 
+
+1) Select the product ID you'd like to purchase 
+2) Enter in the quanity you;d like to purchase 
+3) Ensure Bamazon has enough inventory to fulfill your request 
+
+
--- bamazonCustomer.js
+++ bamazonCustomer.js
@@ -15,11 +15,12 @@ var connected = mysql.createConnection({
   database: "bamazonprods"
 });
 
-// Display bamazon Inventory
+// Display bamazon Inventory. Welcome comes after question.. FIX!
 connected.connect(function(error) {
   if (error) throw error;
-  //   console.log("******WELCOME TO BAMAZON******");
-  //   console.log("-------------------------------------");
+  begin();
+  console.log("****** WELCOME TO BAMAZON ******");
+  console.log("-------------------------------------");
 });
 
 function begin() {
@@ -35,39 +36,52 @@ function begin() {
           res[i].price
       );
     }
+    inquirer
+      .prompt([
+        {
+          type: "input",
+          name: "id",
+          message: "What is the ID of the product you would like to purchase?",
+          validate: function(value) {
+            if (isNaN(value) == false) {
+              return true;
+            } else {
+              return false;
+            }
+          }
+        },
+        {
+          type: "input",
+          message: "How many would you like to purchase?",
+          name: "quantity",
+          validate: function(value) {
+            if (isNaN(value) == false) {
+              return true;
+            } else {
+              return false;
+            }
+          }
+        }
+      ])
+      .then(function(choice) {
+        // User choices for item and cost
+
+        var query =
+          "SELECT product_name, price, stock_qty FROM products WHERE ?";
+        connected.query(query, { item_id: choice.id }, function(error, res) {
+          if (error) throw error;
+
+          var product = res[0];
+          console.log(
+            "Item " + product.product_name + " has been chosen" + ". "
+          );
+          console.log("The cost of this item is: $ " + product.price + ". ");
+        });
+      });
   });
 }
+function customerOrder(id, qtyWant) {
+  connected.query("SELECT * FROM products WHERE item_id " + id, function(error,res) {
 
-// Customer input prompt. FIXED
-inquirer.prompt([
-  {
-    type: "input",
-    name: "id",
-    message: "What is the ID of the product you would like to purchase?",
-    validate: function(value) {
-      if (isNaN(value) == false) {
-        return true;
-      } else {
-        return false;
-      }
-    }
-  },
-  {
-    type: "input",
-    message: "How many would you like to purchase?",
-    name: "quantity",
-    validate: function(value) {
-      if (isNaN(value) == false) {
-        return true;
-      } else {
-        return false;
-      }
-    }
-  }
-]);
-
-begin();
-
-//     if(error) throw error;
-//     console.log("Connection ID: " + connected.threadId + "\n");
-// });
+  })
+}
